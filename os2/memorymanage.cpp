7844c40c2bc9d207d6a8ad19f65445b0ef1ecc24
toyang <582785990@qq.com>
2017-09-13 21:26:44 +0800

    qqq

---
+++ os2/memorymanage.cpp
@@ -0,0 +1,154 @@
+// memorymanage.cpp : Defines the entry point for the console application.
+//
+
+// memoryrelease.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+
+#define MAXFREEROW 50
+
+#define MAXROW 20
+
+typedef struct atab {
+    int processname;  //表示进程名      applytab
+    int len;
+    int flag;        //0存在， 111表示空表项  
+};
+
+typedef struct tab {
+    int add;  // 起始地址在空闲区表标地址，在分配区它表示进程名       freetab
+    int len; ///长度
+    int flag;        //0 表示未使用， 111表示空表项  
+} ftab;
+
+
+int fadd = 0;
+int flen = 0;
+tab freetab[MAXFREEROW] = {0};//初始化全部为零
+atab applytab[MAXROW] = {0};
+int index = 0;//用于记录非空记f录的个数
+int aindex = 0;
+
+void inittab()//先全部初始化为未分配
+{
+    for (int i = 0; i < MAXFREEROW; i++) {
+        freetab[i].flag = 111;
+    }
+
+    for (int i = 0; i < MAXROW; i++) {
+        applytab[i].flag = 111;
+    }
+}
+
+void ReadData() {
+    printf("请输入文件名：\n");
+    char filename[256] = {0};
+    scanf("%s", filename);
+
+    FILE *fp = fopen(filename, "r");
+
+    while (true) {
+        if (fp) {
+            while (!feof(fp))   //结束返回00
+            {
+                fscanf(fp, "%d,%d", &freetab[index].len, &freetab[index].len);
+                printf("%d", freetab[index].add);
+                freetab[index].flag = 0;
+                index++;
+            }
+            break;
+        } else {
+            printf("文件名有误，请输入文件名：\n");
+            scanf("%s", filename);
+        }
+    }
+}
+
+void runbest() {
+    int value = 1000;
+    int i = -1;//记录最小差值ID
+    //printf("%6d",index);
+    for (int x = 0; x < index; x++)//  这里-1是因为只最后一个不需要比较
+    {
+        printf("%6d", freetab[x].len);
+        if (freetab[x].len >= flen) {
+            //	printf(freetab[x].len+"fff");
+            if (value > freetab[x].len - flen) {
+                value = freetab[x].len - flen;
+                i = x;
+            }
+        }
+    }
+    //printf("%d",i);
+    if (i == -1) {
+
+        printf("申请内容过大，没有合适的空间,分配失败\n");
+    } else {
+        applytab[aindex].processname = fadd;
+        applytab[aindex].len = flen;
+        applytab[aindex].flag = 0;
+        aindex++;
+
+        if (freetab[i].len == flen) {
+            for (int n = i + 1; n < index; n++) {
+                memcpy(&freetab[n - 1], &freetab[n], sizeof(tab));
+            }
+            index--;
+        } else {
+            freetab[i].len -= flen;
+        }
+    }
+}
+
+void applymemory() {
+    fadd = 0;
+    flen = 0;
+    printf("请输入申请 存的进程名，长度，以逗号隔开\n");
+    scanf("%d,%d", &fadd, &flen);
+    runbest();//执行最优适配法
+}
+
+void showtab() {
+    printf("内存空闲区内容\n");
+    printf("地址     长度    标志位\n");
+
+    for (int i = 0; i < index; i++)//为方便看可以不显示空表项，题目要求是要显示空表项的
+        //for(int i =0 ;i<MAXFREEROW;i++)
+    {
+        printf("%6d,%6d,%3d\n", freetab[i].add, freetab[i].len, freetab[i].flag);
+    }
+    printf("内存分配表内容\n");
+    printf("进程名    长度    标志位\n");
+
+    //for(int i =0 ;i<MAXROW;i++)//为方便看可以不显示空表项，题目要求是要显示空表项的
+    for (int i = 0; i < aindex; i++) {
+        printf("%6d,%6d,%3d\n", applytab[i].processname, applytab[i].len, applytab[i].flag);
+    }
+}
+
+int _tmain(int argc, _TCHAR *argv[]) {
+    inittab();
+    ReadData();
+    showtab();
+
+    int type = 0;
+
+    while (true) {
+        printf("请输入操作项： 1 申请内存，2 显示内存空间区， 0 退出程序\n");
+        scanf("%d", &type);
+        switch (type) {
+            case 0:
+                return 0;
+            case 1:
+                applymemory();
+                break;
+            case 2:
+                showtab();
+                break;
+            default:
+                printf("输入的是非法值\n");
+        }
+    }
+    return 0;
+}
+
---
+++ os2/memoryrelease.cpp
@@ -0,0 +1,148 @@
+// memoryrelease.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#define MAXROW 50
+
+typedef struct tab {
+    int add;
+    int len;
+    int flag;
+} mtab;
+
+tab freetab[MAXROW] = {0};//初始化全部为零
+
+int index = 0;//用于记录非空记录的个数
+
+void inittab()//先全部初始化为未分配
+{
+    for (int i = 0; i < MAXROW; i++) {
+        freetab[i].flag = 111;
+    }
+}
+
+void ReadData() {
+    printf("请输入文件名：\n");
+    char filename[256] = {0};
+    scanf("%s", filename);
+
+    FILE *fp = fopen(filename, "r");
+
+    while (true) {
+        if (fp) {
+            while (!feof(fp)) {
+                fscanf(fp, "%d,%d", &freetab[index].add, &freetab[index].len);
+                freetab[index].flag = 0;
+                index++;
+            }
+            break;
+        } else {
+            printf("文件名有误，请输入文件名：\n");
+            scanf("%s", filename);
+        }
+    }
+}
+
+void sorttab() {
+    tab tb = {0};
+    for (int i = 0; i < index - 1; i++) {
+        for (int j = i + 1; j < index; j++) {
+            if (freetab[i].add > freetab[j].add) {
+                memcpy(&tb, &freetab[i], sizeof(tab));
+                memcpy(&freetab[i], &freetab[j], sizeof(tab));
+                memcpy(&freetab[j], &tb, sizeof(tab));
+            }
+        }
+    }
+}
+
+int fadd = 0;
+int flen = 0;
+
+void memorymanage() {
+    printf("请输入回收内存的地址，长度，以逗号隔开\n");
+
+    scanf("%d,%d", &fadd, &flen);
+}
+
+void runrelease() {
+    int i = 0;
+    for (; i < index; i++) {
+        if (fadd + flen == freetab[i].add) {
+            freetab[i].add = fadd;
+            freetab[i].len += flen;
+            break;
+        } else if (freetab[i].add + freetab[i].len == fadd) {
+            freetab[i].len += flen;
+            break;
+        }
+    }
+
+    if (i == index) {
+        freetab[index].add = fadd;
+        freetab[index].len = flen;
+        freetab[index].flag = 0;
+    } else                            //如果释放的内存与原来的有相邻，需要检查一下，会不会发生与其它空闲区有相邻
+    {
+        for (int x = 0; x < index - 1; x++)//  这里-1是因为只最后一个不需要比较
+        {
+            for (int y = x + 1; y < index; y++) {
+                if (freetab[x].add + freetab[x].len == freetab[y].add) {
+                    freetab[x].len += freetab[y].len;
+
+                    for (int n = y + 1; n < index; n++) {
+                        memcpy(&freetab[n - 1], &freetab[n], sizeof(tab));
+                    }
+                    index--;
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
+
+void showfreetab() {
+    printf("内容空闲区内容\n");
+    printf("地址      长度    标志位\n");
+
+    for (int i = 0; i < index; i++)//为方便看可以不显示空表项，题目要求是要显示空表项的
+        //for(int i =0 ;i<MAXROW;i++)
+    {
+        printf("%6d,%6d,%3d\n", freetab[i].add, freetab[i].len, freetab[i].flag);
+    }
+
+}
+
+int _tmain(int argc, _TCHAR *argv[]) {
+    inittab();
+    ReadData();
+    sorttab();
+    showfreetab();
+    int type = 0;
+
+    while (true) {
+        printf("请输入操作项： 1 释放内存，2，执行释放内存操作，3 显示内存空间区， 0 退出程序\n");
+
+        scanf("%d", &type);
+
+        switch (type) {
+            case 0:
+                return 0;
+            case 1:
+                memorymanage();
+                break;
+            case 2:
+                runrelease();
+                break;
+            case 3:
+                showfreetab();
+                break;
+            default:
+                printf("输入的是非法值\n");
+        }
+    }
+    return 0;
+}
+
